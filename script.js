83a07ca3c7ec97ed5e51bdc32d506be28fd047d0
emiliano-chp-dev <echp.dev@gmail.com>
2025-05-26 17:21:38 +0300

    Create logic to display one book

--- script.js
+++ script.js
@@ -13,12 +13,39 @@ function main() {
     modalElement.classList.toggle('open');
   }
 
+  const bookExample = {
+    bookTitle: 'The Hobbit',
+    bookAuthor: 'J. R. R. Tolkien',
+    bookPubYear: 1937,
+    bookPages: 321,
+    bookStatus: 'unread',
+  };
+
+  function displayBook(book) {
+    const { bookTitle, bookAuthor, bookPubYear, bookPages, bookStatus } = book;
+    const markdown = `
+    <div class="card card-book">
+      <h4>${bookTitle}</h4>
+      <p>Author: <em>${bookAuthor}</em></p>
+      <p>Published: <em>${bookPubYear}</em></p>
+      <p>Pages: <em>${bookPages}</em></p>
+      <div class="button-container">
+        <button class="btn btn-status read">${bookStatus.toUpperCase()}</button>
+        <button class="btn btn-delete-book">DELETE</button>
+      </div>
+    </div>
+    `;
+
+    bookContainerElement.insertAdjacentHTML('beforeend', markdown);
+  }
+
   function attachHandlers(arr) {
     arr.map(el => {
       el.addEventListener('click', toggleModalVisibility);
     });
   }
 
+  displayBook(bookExample);
   attachHandlers(modalBtns);
 }
 
